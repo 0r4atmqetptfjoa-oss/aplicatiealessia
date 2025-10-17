d2173ac28188b088ac2cda524e08ccb938256295
0r4atmqetptfjoa-oss <0r4atmqetptfjoa@gmail.com>
2025-10-18 00:20:01 +0300

    fix

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,28 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get mainMenuTitle => 'Alessia\'s World';
+
+  @override
+  String get sounds => 'Sounds';
+
+  @override
+  String get instruments => 'Instruments';
+
+  @override
+  String get songs => 'Songs';
+
+  @override
+  String get stories => 'Stories';
+
+  @override
+  String get games => 'Games';
+}
---
+++ lib/l10n/app_localizations_ro.dart
@@ -0,0 +1,28 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Romanian Moldavian Moldovan (`ro`).
+class AppLocalizationsRo extends AppLocalizations {
+  AppLocalizationsRo([String locale = 'ro']) : super(locale);
+
+  @override
+  String get mainMenuTitle => 'Lumea Alessiei';
+
+  @override
+  String get sounds => 'Sunete';
+
+  @override
+  String get instruments => 'Instrumente';
+
+  @override
+  String get songs => 'Cântece';
+
+  @override
+  String get stories => 'Povești';
+
+  @override
+  String get games => 'Jocuri';
+}
