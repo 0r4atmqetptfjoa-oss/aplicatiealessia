4cdd46df2cfe0e25f07594eaae90c25428aead97
0r4atmqetptfjoa-oss <0r4atmqetptfjoa@gmail.com>
2025-10-30 20:35:47 +0200

    aa

---
+++ lib/src/features/games/memory_game_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MemoryGameScreen extends StatelessWidget {
+  const MemoryGameScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Memory Game')),
+      body: const Center(child: Text('Memory Game - Under Construction')),
+    );
+  }
+}
