0830540b0a358939790b849a1e973dcdea9b7fe7
0r4atmqetptfjoa-oss <0r4atmqetptfjoa@gmail.com>
2025-10-30 20:59:53 +0200

    update

---
+++ lib/src/features/games/sorting_animals_game_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SortingAnimalsGameScreen extends StatelessWidget {
+  const SortingAnimalsGameScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Sorting Animals Game')),
+      body: const Center(child: Text('Sorting Animals Game - Under Construction')),
+    );
+  }
+}
