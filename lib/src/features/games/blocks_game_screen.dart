3f985346549ede2e7dec0ffc40640a94438a5b7a
0r4atmqetptfjoa-oss <0r4atmqetptfjoa@gmail.com>
2025-10-30 21:00:45 +0200

    Create blocks_game_screen.dart

---
+++ lib/src/features/games/blocks_game_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BlocksGameScreen extends StatelessWidget {
+  const BlocksGameScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Blocks Game')),
+      body: const Center(child: Text('Blocks Game - Under Construction')),
+    );
+  }
+}
