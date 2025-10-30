3df0108156c8c39c1405e2e36dd7267098b66cb5
0r4atmqetptfjoa-oss <0r4atmqetptfjoa@gmail.com>
2025-10-30 22:29:57 +0200

    aa

---
+++ lib/src/features/games/instruments_game_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class InstrumentsGameScreen extends StatelessWidget {
+  const InstrumentsGameScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Instruments Game')),
+      body: const Center(child: Text('Instruments Game - Under Construction')),
+    );
+  }
+}
